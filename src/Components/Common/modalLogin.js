bca2fbab4ccc95de5803f93c7860dc5a1b2d8a87
Maximiliano Diaz Huerta <diazhuerta93@gmail.com>
2020-09-15 23:41:11 -0300

    -

--- src/Components/Common/modalLogin.js
+++ src/Components/Common/modalLogin.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import "./ModalLogin.css";
+import "./modalLogin.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import Alert from "react-bootstrap/Alert";
